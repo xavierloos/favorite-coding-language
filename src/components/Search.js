7585d1e140c3fb6e2c6a49eb81edaea0b04717c6
xavierloos <xavier.loos04@gmail.com>
2021-06-06 19:01:56 +0100

    Fetching the data from the API

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,7 +1,14 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {Form} from 'semantic-ui-react'
 
 function Search() {
+  useEffect(() => {
+    fetch("https://api.github.com/users/example")
+      .then(res => res.json())
+      .then(data => {
+        console.log(data)
+      });
+  }, [])
   return (
     <div>
       <Form>
