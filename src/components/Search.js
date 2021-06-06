8351990412129b0c76106d45079335393bb67b90
xavierloos <xavier.loos04@gmail.com>
2021-06-06 21:48:09 +0100

    Get the favorite language

--- src/components/Search.js
+++ src/components/Search.js
@@ -5,18 +5,34 @@ function Search() {
   const [result, setResult] = useState("")
 
   useEffect(() => {
-    fetch("https://api.github.com/users/xavierloos/repos")
+    fetch("https://api.github.com/users/AArnott/repos")
       .then(res => res.json())
       .then(data => {
         const languages = data.map(language => (
           language.language
         ))
-        setData(languages)
+        favLanguage(languages)
       });
   }, [])
 
   const setData = result => {
     setResult(result)
+  }
+
+  const favLanguage = lang =>{
+    var countLang = {}, counter = 0, favorite = [];
+
+    lang.forEach(item => {
+      (!countLang[item]) ? countLang[item] = 1 :  countLang[item]++; 
+      if (countLang[item] > counter) {
+        counter = countLang[item];
+        favorite = [item];
+      } else if (countLang[item] === counter) {
+        favorite.push(item);
+      }
+    });
+
+    setData(favorite)
   }
 
   return (
