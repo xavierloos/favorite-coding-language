c761d23f8b226c470df74c9447fd82426c5a8810
xavierloos <xavier.loos04@gmail.com>
2021-06-07 16:39:28 +0100

    Update

--- src/App.js
+++ src/App.js
@@ -2,18 +2,14 @@ import React from 'react';
 import './App.css';
 import Header from "./components/Header";
 import Search from './components/Search';
-import Github from './hooks/Github';
 
-class App extends React.Component {
-  render() {
-    return (
-      <div className="App">
-        <Header />
-        <Search />
-        <Github />
-      </div>
-    );
-  }
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Search />
+    </div>
+  );
 }
 
 export default App;
