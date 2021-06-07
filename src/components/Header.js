4ac8c832ea4bb2fe9bcfbf239c28e52a4f9f3097
xavierloos <xavier.loos04@gmail.com>
2021-06-07 18:02:30 +0100

    Jumbotron

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import { Jumbotron } from 'reactstrap';
 function Header() {
   return (
-    <h1>
-      Search for a Github username
-    </h1>
+    <Jumbotron>
+      <div className="container">
+        <div className="row row-header">
+          <div className="col-12 col-sm-6">
+            <h1>Github Username</h1>
+            <p>Getting inpiration from github users favorite coding language.</p>
+          </div>
+        </div>
+      </div>
+    </Jumbotron>
   );
 }
 
