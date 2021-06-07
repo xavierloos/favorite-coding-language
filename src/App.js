fe97913360f39a0b7443b4cee8eacee2de35d5c3
xavierloos <xavier.loos04@gmail.com>
2021-06-07 16:38:18 +0100

    Trying Hooks

--- src/App.js
+++ src/App.js
@@ -2,14 +2,18 @@ import React from 'react';
 import './App.css';
 import Header from "./components/Header";
 import Search from './components/Search';
+import Github from './hooks/Github';
 
-function App() {
-  return (
-    <div className="App">
-      <Header />
-      <Search />
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <Header />
+        <Search />
+        <Github />
+      </div>
+    );
+  }
 }
 
 export default App;
