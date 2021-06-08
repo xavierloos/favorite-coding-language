4f5a4c7117d4cdca67ccbefb4abb6c2cb8507094
xavierloos <xavier.loos04@gmail.com>
2021-06-08 13:11:19 +0100

    Import card from bootstrap

--- src/components/Usercard.js
+++ src/components/Usercard.js
@@ -1,9 +1,22 @@
 import React from 'react';
 import '../App.css';
+import { Button } from 'reactstrap';
+import Card from 'react-bootstrap/Card'
 function Usercard(language) {
   return (
     <div>
       <h1>Usercard</h1>
+      <Card style={{ width: '18rem' }}>
+        <Card.Img variant="top" src="holder.js/100px180" />
+        <Card.Body>
+          <Card.Title>Card Title</Card.Title>
+          <Card.Text>
+            Some quick example text to build on the card title and make up the bulk of
+            the card's content.
+        </Card.Text>
+          <Button variant="primary">Go somewhere</Button>
+        </Card.Body>
+      </Card>
       <div>{language.language}</div>
     </div>
   );
