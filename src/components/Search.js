1cc9d58cdd4812eede9859f7835ac43bf092dc9c
xavierloos <xavier.loos04@gmail.com>
2021-06-08 16:25:06 +0100

    Handling errors

--- src/components/Search.js
+++ src/components/Search.js
@@ -14,20 +14,26 @@ function Search() {
   const [blog, setBlog] = useState("")
   const [git, setGit] = useState("")
   const [searchInput, setSearchInput] = useState("")
+  const [error, setError] = useState("")
 
   useEffect(() => {
     fetch("https://api.github.com/users/xavierloos/repos")
       .then(res => res.json())
       .then(data => {
-        const languages = data.map(language => (
-          language.language
-        ))
-        favLanguage(languages)
+        if (data.message) {
+          setError(data.message)
+        } else {
+          const languages = data.map(language => (
+            language.language
+          ))
+          favLanguage(languages)
+          setError(null)
+        }
       });
     fetch("https://api.github.com/users/xavierloos")
       .then(res => res.json())
       .then(data => {
-        setUserData(data.login, data.avatar_url, data.name, data.location, data.blog, data.html_url)
+        (data.message) ? setError(data.message) : setUserData(data.login, data.avatar_url, data.name, data.location, data.blog, data.html_url);
       });
   }, [])
 
@@ -66,15 +72,21 @@ function Search() {
     fetch(`https://api.github.com/users/${searchInput}/repos`)
       .then(res => res.json())
       .then(data => {
-        const languages = data.map(language => (
-          language.language
-        ))
-        favLanguage(languages)
+        if (data.message) {
+          setError(data.message)
+        } else {
+          const languages = data.map(language => (
+            language.language
+          ))
+          favLanguage(languages)
+          setError(null)
+        }
+
       });
     fetch(`https://api.github.com/users/${searchInput}`)
       .then(res => res.json())
       .then(data => {
-        setUserData(data.login, data.avatar_url, data.name, data.location, data.blog, data.html_url)
+        (data.message) ? setError(data.message) : setUserData(data.login, data.avatar_url, data.name, data.location, data.blog, data.html_url)
       });
   }
 
@@ -86,7 +98,7 @@ function Search() {
             <div className="col-12 col-sm-12 mx-auto align-content-center ">
               <Form onSubmit={handleSubmit}>
                 <Form.Group>
-                  <Form.Input placeholder="Search for a github username" name="username" onChange={handleSearch} />
+                  <Form.Input placeholder="Search for a github username" name="username" onChange={handleSearch} required />
                   <Form.Button content="Search" />
                 </Form.Group>
               </Form>
@@ -97,32 +109,34 @@ function Search() {
       <div className="container">
         <div className="row row-header ">
           <div className="col-12 col-sm-12 d-flex justify-content-center">
-            <Card style={{ width: '18rem' }}>
-              <div className="card-header">
-                <Card.Img className="avatar" src={avatar} />
-              </div>
-              <Card.Body>
-                <Card.Title className="d-flex justify-content-center">{name}</Card.Title>
-                <Card.Text>
-                  <div className="info">
-                    @{username}'s favorite language is <span className="lang">{result}</span>!
+            {error ? (<h1>Username not found, please try another username</h1>) : (
+              <Card style={{ width: '18rem' }}>
+                <div className="card-header">
+                  <Card.Img className="avatar" src={avatar} />
+                </div>
+                <Card.Body>
+                  <Card.Title className="d-flex justify-content-center">{name}</Card.Title>
+                  <Card.Text>
+                    <div className="info">
+                      @{username}'s favorite language is <span className="lang">{result}</span>!
                   </div>
-                  <div className="col-12 col-sm-12 d-flex justify-content-between">
-                    <div className="text-muted" >
-                      <FaMapMarkerAlt />{location}
+                    <div className="col-12 col-sm-12 d-flex justify-content-between">
+                      <div className="text-muted" >
+                        <FaMapMarkerAlt />{location}
+                      </div>
+                      <div >
+                        <a href={blog} alt="Go to the blog link" className="text-muted"><FaLink /> Blog</a>
+                      </div>
                     </div>
-                    <div >
-                      <a href={blog} alt="Go to the blog link" className="text-muted"><FaLink /> Blog</a>
+                    <div>
+                      <a href={git} alt="Go to GitHub">
+                        <Button className="github-button">Open on Github</Button>
+                      </a>
                     </div>
-                  </div>
-                  <div>
-                    <a href={git} alt="Go to GitHub">
-                      <Button className="github-button">Open on Github</Button>
-                    </a>
-                  </div>
-                </Card.Text>
-              </Card.Body>
-            </Card>
+                  </Card.Text>
+                </Card.Body>
+              </Card>
+            )}
           </div>
         </div>
       </div>
