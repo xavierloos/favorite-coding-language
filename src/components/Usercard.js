7de3ff19364ba8234818c4a785186d4160dc470e
xavierloos <xavier.loos04@gmail.com>
2021-06-08 13:14:02 +0100

    Container

--- src/components/Usercard.js
+++ src/components/Usercard.js
@@ -4,20 +4,21 @@ import { Button } from 'reactstrap';
 import Card from 'react-bootstrap/Card'
 function Usercard(language) {
   return (
-    <div>
-      <h1>Usercard</h1>
-      <Card style={{ width: '18rem' }}>
-        <Card.Img variant="top" src="holder.js/100px180" />
-        <Card.Body>
-          <Card.Title>Card Title</Card.Title>
-          <Card.Text>
-            Some quick example text to build on the card title and make up the bulk of
-            the card's content.
-        </Card.Text>
-          <Button variant="primary">Go somewhere</Button>
-        </Card.Body>
-      </Card>
-      <div>{language.language}</div>
+    <div className="container">
+      <div className="row row-header">
+        <div className="col-3 col-sm-3">
+          <Card style={{ width: '18rem' }}>
+            <Card.Img variant="top" src="holder.js/100px180" />
+            <Card.Body>
+              <Card.Title>Card Title</Card.Title>
+              <Card.Text>
+                {language.language}
+              </Card.Text>
+              <Button variant="primary">Go somewhere</Button>
+            </Card.Body>
+          </Card>
+        </div>
+      </div>
     </div>
   );
 }
